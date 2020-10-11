4778d3a59cf8d0672b8f100bb4dbb0fafcd84e8f
tund-hcmue <tund.fit@gmail.com>
2020-10-12 00:31:37 +0700

    submit Lab01 - 05 test case

---
+++ Test Suites/TS_001_Lab01.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_001_Lab01</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>56e40404-bbb0-4c7a-8e19-82ffd88f3a2b</testSuiteGuid>
+   <testCaseLink>
+      <guid>c5ef135b-539b-45fc-88f8-6541cdf7d8a8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_001_LoginUserPassNull</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9e637c6f-cb2c-41ff-999f-d59f05c3d950</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_002_LoginPassFail</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>bf05b69f-ee16-470b-a3d4-f78103a950a5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_003_LoginPassNull</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>11eb029a-8c62-46b2-b0a4-792e72aca9b4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_004_LoginSuccesful</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d79e8387-6822-4b64-9c7a-1f40e2162402</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_005_LoginUserPassFail</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
